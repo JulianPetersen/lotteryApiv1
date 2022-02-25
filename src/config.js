313b6532bbb348bdac5a0aec1ad393b55f0f1583
JulianPetersen <julianpetersen92@gmail.com>
2022-02-25 17:55:16 -0300

    connect to database

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config();
 
 
 export default{
-    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://jdpeters:26092348@cluster0.jaeuy.mongodb.net/test',
+    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://jdpeters:26092348@cluster0.jaeuy.mongodb.net/test?retryWrites=true&w=majority',
     port: process.env.APP_PORT || '3000',
     host: process.env.APP_HOST || 'http://localhost'
 }
