8157973ef59fda766d72f7a9fc37a77fefd7c4bf
JulianPetersen <julianpetersen92@gmail.com>
2022-03-02 09:13:05 -0300

    version con atlas

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default{
     //Desarrollo
     //mongodbURL: process.env.MONGODB_URI || 'mongodb://localhost/lotteryapi',
     //Despliegue
-    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://jdpeters:26092348@cluster0.jaeuy.mongodb.net/lotteryapp?retryWrites=true&w=majority',
+    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://jdpeters:26092348@cluster0.jaeuy.mongodb.net/test',
     port: process.env.APP_PORT || '3000',
     host: process.env.APP_HOST || 'http://localhost'
 }
