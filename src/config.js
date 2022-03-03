ea7c4462e2a32039033cc2913b38d060affee8ab
JulianPetersen <julianpetersen92@gmail.com>
2022-03-03 15:58:30 -0300

    version de desarrollo con aws

--- src/config.js
+++ src/config.js
@@ -8,6 +8,13 @@ export default{
     //produccion:
     mongodbURL: process.env.APP_ROMOTE_HOST || 'mongodb://localhost/lotteryapi',
     port: process.env.APP_PORT || '3000',
-    host: process.env.APP_HOST || 'http://localhost'
+    host: process.env.APP_HOST || 'http://localhost',
+
+    aws:{
+        accessKeyID: process.env.ACCESS_KEY_ID,
+        privateAccessKey: process.env.PRIVATE_ACCESS_KEY,
+        s3BucketName: process.env.S3_BUCKET_NAME
+    }
+    
 }
 
