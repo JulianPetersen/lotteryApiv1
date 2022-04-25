2490c0a7a3e337bb2a2baedb620b7458980bc9bd
JulianPetersen <julianpetersen92@gmail.com>
2022-04-25 18:12:20 -0300

    asd

---
+++ dist/controllers/igcomment.controller.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getcomment = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
+
+var fetchInstagram = require("webscraper_for_instagram");
+
+var getcomment = /*#__PURE__*/function () {
+  var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res, reject) {
+    var post;
+    return _regenerator["default"].wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.prev = 0;
+            _context.next = 3;
+            return fetchInstagram("https://www.instagram.com/p/".concat(req.params.id, "/"));
+
+          case 3:
+            post = _context.sent;
+            res.send(post);
+            _context.next = 10;
+            break;
+
+          case 7:
+            _context.prev = 7;
+            _context.t0 = _context["catch"](0);
+            res.status(500).json({
+              message: 'su link no corresponde a un sorteo de instagram'
+            });
+
+          case 10:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, null, [[0, 7]]);
+  }));
+
+  return function getcomment(_x, _x2, _x3) {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+exports.getcomment = getcomment;
